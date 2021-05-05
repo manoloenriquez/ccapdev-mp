dfde26ff233db63eba19bb519c9f4fbac963aa4e
Manolo Enriquez <noloenriquez@gmail.com>
2021-05-05 14:02:51 +0800

    updated post and comment schema

--- server/model/commentModel.js
+++ server/model/commentModel.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
-  id: String,
-  posterID: String,
-  postID: String,
-  content: String,
-  date: Date
+  userID: {
+    type: String,
+    required: true
+  },
+  postID: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  votes: {
+    type: Number,
+    default: 0
+  }
 })
 
 module.exports = mongoose.model('Comment', commentSchema)
--- server/model/postModel.js
+++ server/model/postModel.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-  slug: String,
-  title: String,
-  content: String,
-  auhorID: String,
-  date: Date,
-  votes: Number
+  slug: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  auhorID: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  votes: {
+    type: Number,
+    default: 0
+  }
 })
 
 module.exports = mongoose.model('Post', postSchema)
