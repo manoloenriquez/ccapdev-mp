d700a7ad693d3eebea3354ed7fc392a2f544bcb4
Manolo Enriquez <noloenriquez@gmail.com>
2021-05-24 23:55:40 +0800

    added search results page

--- routes/search.js
+++ routes/search.js
@@ -5,8 +5,22 @@ const db = require('../model/db')
 
 const Post = require('../model/postModel')
 
-router.get('/check', async (req, res) => {
+router.get('/', async (req, res) => {
+  let posts = await db.get(Post, '', { 'title': { '$regex': req.query.search, '$options': 'i' } })
 
+  posts.forEach((post) => {
+    post.date = post.date.toDateString()
+  })
+
+  res.render('search', {
+    title: 'Search Results',
+    loggedIn: req.session.loggedIn,
+    username: req.session.username,
+    posts: posts
+  })
+})
+
+router.get('/check', async (req, res) => {
   let result = await db.get(Post, 'slug title', { "title": { "$regex": req.query.content, "$options": "i" } })
 
   res.send(result)
